1f4ffc37ad7f9a70dfa7515af06bd477cc29225d
Valey88 <146131996+Valey88@users.noreply.github.com>
2025-10-05 23:33:05 +0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// import imageOptimizer from "vite-plugin-image-optimizer";
+import compression from "vite-plugin-compression";
+import { resolve } from "path";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // imageOptimizer({
+    //   png: { quality: 80 }, // Оптимизация PNG
+    //   jpeg: { quality: 80 }, // Оптимизация JPEG
+    //   webp: { quality: 80 }, // Конвертация в WebP
+    // }),
+    compression({
+      algorithm: "brotliCompress",
+    }),
+  ],
+  build: {
+    outDir: "build", // Устанавливаем выходную директорию
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
